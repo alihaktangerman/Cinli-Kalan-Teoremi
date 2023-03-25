f9fa0f5b759ae6d28ddc0aab848effa4e006465f
Ali Haktan <107588410+alihaktangerman@users.noreply.github.com>
2023-03-25 23:28:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from math import gcd
 
 def solve(a, b):
     assert(gcd(a[1], b[1]) == 1)
+    if (a[0] >= a[1]): a[0] %= a[1]
+    if (b[0] >= b[1]): b[0] %= b[1]
     for i in range(a[0], a[1]*b[1], a[1]):
         if i % b[1] == b[0]:
             return i, a[1]*b[1]
