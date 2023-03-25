56a755ab1104ed22a8b63680f5c6de2f8a04e221
Ali Haktan <107588410+alihaktangerman@users.noreply.github.com>
2023-03-25 15:11:22 +0300

    Create main.py
    Chinese remainder theorem solver.

---
+++ main.py
@@ -0,0 +1,15 @@
+from functools import reduce
+from math import gcd
+
+
+def solve(a, b):
+    assert(gcd(a[1], b[1]) == 1)
+    for i in range(a[0], a[1]*b[1], a[1]):
+        if i % b[1] == b[0]:
+            return i, a[1]*b[1]
+
+
+def crt(*args): return reduce(solve, args)
+
+
+print(crt((1, 2), (2, 3), (3, 5)))
