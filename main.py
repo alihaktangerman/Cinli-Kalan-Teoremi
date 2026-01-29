05e0bbff74a9d400e165d34458b3ce9025166805
Ali Haktan <107588410+alihaktangerman@users.noreply.github.com>
2026-01-29 13:59:42 +0300

    Fix range checks for a[0] and b[0] in solve function
    Refactor conditions to ensure a[0] and b[0] are within valid ranges.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from math import gcd
 
 def solve(a, b):
     assert(gcd(a[1], b[1]) == 1)
-    if not 0 <= a[0] < a[1]: a[0] %= a[1]
-    if not 0 <= b[0] < b[1]: b[0] %= b[1]
     for i in range(a[0], a[1]*b[1], a[1]):
         if i % b[1] == b[0]:
             return i, a[1]*b[1]
